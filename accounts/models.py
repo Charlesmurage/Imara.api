ed71a73a7a27c6c4629959685760bf8d3fec8d09
Charles maina <muragemaina09@gmail.com>
2019-10-02 11:34:17 +0300

    added abstract user

--- accounts/models.py
+++ accounts/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 class CustomUser(AbstractUser):
     #additional fields here
+    is_sponsor = models.BooleanField(default=False)
+    is_creator = models.BooleanField(default=False)
 
     def __str__(self):
         return self.email
