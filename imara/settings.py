2c7308e016a3fc809c157d37a4154eee3628b35f
yunus <yunus@competamillman.co.ke>
2019-10-03 17:57:16 +0300

    chore(database): change db to sqlite

--- imara/settings.py
+++ imara/settings.py
@@ -85,10 +85,8 @@ WSGI_APPLICATION = 'imara.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'iapi',
-        'USER': 'charles',
-    'PASSWORD':'chweezyy94',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
